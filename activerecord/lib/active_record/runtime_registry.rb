b1f2e76a617b252f81d1907e56c35b84a7a76e19
Jean Boussier <jean.boussier@gmail.com>
2025-09-23 15:42:53 +0200

    Micro optimize ActiveRecord::RuntimeRegistry
    Access the IsolatedExecutionState once instead of once for every metric.

--- activerecord/lib/active_record/runtime_registry.rb
+++ activerecord/lib/active_record/runtime_registry.rb
@@ -7,77 +7,80 @@ module ActiveRecord
   #
   # returns the connection handler local to the current unit of execution (either thread of fiber).
   module RuntimeRegistry # :nodoc:
+    class Stats
+      attr_accessor :sql_runtime, :async_sql_runtime, :queries_count, :cached_queries_count
+
+      def initialize
+        @sql_runtime = 0.0
+        @async_sql_runtime = 0.0
+        @queries_count = 0
+        @cached_queries_count = 0
+      end
+
+      alias_method :reset, :initialize
+
+      def reset_runtimes
+        rt, self.sql_runtime = sql_runtime, 0.0
+        self.async_sql_runtime = 0.0
+        rt
+      end
+
+      def reset_queries_count
+        qc = queries_count
+        self.queries_count = 0
+        qc
+      end
+
+      def reset_cached_queries_count
+        qc = cached_queries_count
+        self.cached_queries_count = 0
+        qc
+      end
+    end
+
     extend self
 
-    def sql_runtime
-      ActiveSupport::IsolatedExecutionState[:active_record_sql_runtime] ||= 0.0
+    def record(query_name, runtime, cached: false, async: false, lock_wait: nil)
+      stats = self.stats
+
+      unless query_name == "TRANSACTION" || query_name == "SCHEMA"
+        stats.queries_count += 1
+        stats.cached_queries_count += 1 if cached
+      end
+
+      if async
+        stats.async_sql_runtime += (runtime - lock_wait)
+      end
+      stats.sql_runtime += runtime
     end
 
-    def sql_runtime=(runtime)
-      ActiveSupport::IsolatedExecutionState[:active_record_sql_runtime] = runtime
-    end
-
-    def async_sql_runtime
-      ActiveSupport::IsolatedExecutionState[:active_record_async_sql_runtime] ||= 0.0
-    end
-
-    def async_sql_runtime=(runtime)
-      ActiveSupport::IsolatedExecutionState[:active_record_async_sql_runtime] = runtime
-    end
-
-    def queries_count
-      ActiveSupport::IsolatedExecutionState[:active_record_queries_count] ||= 0
-    end
-
-    def queries_count=(count)
-      ActiveSupport::IsolatedExecutionState[:active_record_queries_count] = count
-    end
-
-    def cached_queries_count
-      ActiveSupport::IsolatedExecutionState[:active_record_cached_queries_count] ||= 0
-    end
-
-    def cached_queries_count=(count)
-      ActiveSupport::IsolatedExecutionState[:active_record_cached_queries_count] = count
+    def stats
+      ActiveSupport::IsolatedExecutionState[:active_record_runtime] ||= Stats.new
     end
 
     def reset
-      reset_runtimes
-      reset_queries_count
-      reset_cached_queries_count
+      stats.reset
     end
 
     def reset_runtimes
-      rt, self.sql_runtime = sql_runtime, 0.0
-      self.async_sql_runtime = 0.0
-      rt
+      stats.reset_runtimes
     end
 
     def reset_queries_count
-      qc = queries_count
-      self.queries_count = 0
-      qc
+      stats.reset_queries_count
     end
 
     def reset_cached_queries_count
-      qc = cached_queries_count
-      self.cached_queries_count = 0
-      qc
+      stats.reset_cached_queries_count
     end
   end
 end
 
 ActiveSupport::Notifications.monotonic_subscribe("sql.active_record") do |name, start, finish, id, payload|
-  query_name = payload[:name]
-  unless query_name == "TRANSACTION" || query_name == "SCHEMA"
-    ActiveRecord::RuntimeRegistry.queries_count += 1
-    ActiveRecord::RuntimeRegistry.cached_queries_count += 1 if payload[:cached]
-  end
-
-  runtime = (finish - start) * 1_000.0
-
-  if payload[:async]
-    ActiveRecord::RuntimeRegistry.async_sql_runtime += (runtime - payload[:lock_wait])
-  end
-  ActiveRecord::RuntimeRegistry.sql_runtime += runtime
+  ActiveRecord::RuntimeRegistry.record(
+    payload[:name],
+    (finish - start) * 1_000.0,
+    async: payload[:async],
+    lock_wait: payload[:lock_wait],
+  )
 end
